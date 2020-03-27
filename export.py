02505c8a62095d7905b5ca393247d29247240e6d
Moritz <mrinow.dev@gmail.com>
2020-03-28 00:23:18 +0100

    init

---
+++ export.py
@@ -0,0 +1,33 @@
+import os
+import json
+import requests
+import urllib
+
+try:
+  with open('config.json') as f:
+    conf = json.load(f)
+except IOError:
+  print('Config file does not exist')
+  exit(1)
+
+url = 'https://favro.com/api/v1/'
+user = conf['user']
+org = conf['organization']
+token = conf['token']
+auth = (user, token)
+
+def get_url(method):
+  return url + method
+    
+def get_collections():
+  path = get_url("collections")
+  r = requests.get(path, headers={
+    "organizationId": org,
+    "archived": "true"archive
+  }, auth=auth).content
+  return json.loads(r)['entities']
+
+collections = get_collections()
+
+for collection in collections:
+  print(collection['name'])
